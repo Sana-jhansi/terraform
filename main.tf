dfc23c921ce638861a310c8c5a8c3005dfdb167f
jhansi <sanajhansi123@gmail.com>
2024-09-15 09:11:09 +0530

    Tag add

--- main.tf
+++ main.tf
@@ -24,5 +24,8 @@ resource "aws_s3_bucket" "jhansi" {
 resource "aws_instance" "example"{
     ami = var.ami
     instance_type = var.instance_type
-    name = var.name
+    
+    tags = {
+      Name = "var.name"
+    }
 }
