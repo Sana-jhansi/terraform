a12e33a98ddb47c1043dc63f3ac700e8671be2da
jhansi <sanajhansi123@gmail.com>
2024-09-16 18:09:07 +0530

    add

--- resource-lock/conditions/main.tf
+++ resource-lock/conditions/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test" {
     ami = "ami-0e86e20dae9224db8"
-    instance_type = var.instance_type == "t2.micro" ? "t3.*" : var.instance_type
+    instance_type = var.instance_type == "t2.micro" ? "t3*" : var.instance_type
     
 }
 
